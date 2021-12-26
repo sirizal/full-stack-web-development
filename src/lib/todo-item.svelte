ba8bebb97dd8a037380eacc713c0460cb7bfe2c4
sirizal <sirizal@gmail.com>
2021-12-26 02:35:03 +0000

    Develop HTML Structure.

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,13 @@
+<div>
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done or not done">Done/Notdone</button>
+    </form>
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save to do">Save</button>
+    </form>
+    <form action="" method="">
+        <button aria-label="Delete to do">Delete</button>
+    </form>
+</div>
